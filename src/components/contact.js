7cfc74128922872109d24f502129894729820e1e
Dominik Gmiterko <d.gmiterko@gmail.com>
2021-03-14 02:01:55 +0100

    Style update

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
+import { Link } from "gatsby";
 import React, { Component } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import MessengerMessageUs from 'react-messenger-message-us';
 import { FormattedMessage } from 'react-intl';
 
@@ -7,69 +8,76 @@ export default class service extends Component {
   render() {
     const { site } = this.props;
     return (
-      <Container className="p-3">
-        <Row>
-          <Col md={12} className="p-3">
-            <h2 id="Contact">
-              <FormattedMessage id="title.contact" defaultMessage="Contact" />
-            </h2>
-          </Col>
-        </Row>
-        <dl>
+      <div className="bg-primary">
+        <Container className="p-3">
+          <Row>
+            <Col md={12} className="p-3">
+              <h2 id="Contact">
+                <FormattedMessage id="title.contact" defaultMessage="Contact" />
+              </h2>
+              <div className="text-center">
+                <Button as={Link} to="/#Register" variant="danger" type="submit" size="lg">
+                  <FormattedMessage id="contact.register" defaultMessage="Submit" />
+                </Button>
+              </div>
+            </Col>
+          </Row>
+          <dl>
 
-          {
-            site.email &&
-            <Row className="justify-content-center align-items-center">
-              <dt className="col-sm-2">
-                <FormattedMessage id="contact.email" defaultMessage="Email" />
-              </dt>
-              <dd className="col-sm-4">{site.email}</dd>
-            </Row>
-          }
+            {
+              site.email &&
+              <Row className="justify-content-center align-items-center">
+                <dt className="col-sm-2">
+                  <FormattedMessage id="contact.email" defaultMessage="Email" />
+                </dt>
+                <dd className="col-sm-4">{site.email}</dd>
+              </Row>
+            }
 
-          {
-            site.fbPageId && site.fbAppId &&
-            <Row className="justify-content-center align-items-center">
-              <dt class="col-sm-2">
-                <FormattedMessage id="contact.messenger" defaultMessage="Messenger" />
-              </dt>
-              <dd class="col-sm-4">
-                <MessengerMessageUs pageId={site.fbPageId} appId={site.fbAppId} size="large" />
-              </dd>
-            </Row>
-          }
+            {
+              site.fbPageId && site.fbAppId &&
+              <Row className="justify-content-center align-items-center">
+                <dt class="col-sm-2">
+                  <FormattedMessage id="contact.messenger" defaultMessage="Messenger" />
+                </dt>
+                <dd class="col-sm-4">
+                  <MessengerMessageUs pageId={site.fbPageId} appId={site.fbAppId} size="large" />
+                </dd>
+              </Row>
+            }
 
-          {
-            (site.facebook || site.instagram) &&
-            <Row className="justify-content-center align-items-center">
-              <dt class="col-sm-2">
-                <FormattedMessage id="contact.socials" defaultMessage="Socials" />
-              </dt>
-              <dd class="col-sm-4">
-                <ul className="social">
-                  <li>
-                    <a
-                      className="fab fa-facebook-f"
-                      href={site.facebook}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >Facebook</a>
-                  </li>
-                  <li>
-                    <a
-                      className="fab fa-instagram"
-                      href={site.instagram}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >Instagram</a>
-                  </li>
-                </ul>
-              </dd>
-            </Row>
-          }
+            {
+              (site.facebook || site.instagram) &&
+              <Row className="justify-content-center align-items-center">
+                <dt class="col-sm-2">
+                  <FormattedMessage id="contact.socials" defaultMessage="Socials" />
+                </dt>
+                <dd class="col-sm-4">
+                  <ul className="social">
+                    <li>
+                      <a
+                        className="fab fa-facebook-f"
+                        href={site.facebook}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >Facebook</a>
+                    </li>
+                    <li>
+                      <a
+                        className="fab fa-instagram"
+                        href={site.instagram}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >Instagram</a>
+                    </li>
+                  </ul>
+                </dd>
+              </Row>
+            }
 
-        </dl>
-      </Container>
+          </dl>
+        </Container>
+      </div>
     );
   }
 }
--- src/components/register.js
+++ src/components/register.js
@@ -9,108 +9,110 @@ export default class Register extends Component {
   render() {
     const { site, faculties } = this.props;
     return (
-      <Container className="p-3">
-        <Row className="justify-content-center align-items-center">
-          <Col md={12} className="p-3">
-            <h2 id="Register">
-              <FormattedMessage id="title.register" defaultMessage="Register" />
-            </h2>
-            <Markdown value={site.registerDescription} />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} className="p-3">
-            <Form> {/* TODO add https://formspree.io/ */}
-              <Form.Group controlId="registerEmail">
-                <FormattedMessage id="register.email" defaultMessage="Email">
-                  {(l_email) => (
-                    <>
-                      <Form.Label>
-                        {l_email}
-                      </Form.Label>
-                      <Form.Control type="email" placeholder={l_email} />
-                    </>
-                  )}
-                </FormattedMessage>
-              </Form.Group>
+      <div className="bg-primary">
+        <Container className="p-3">
+          <Row className="justify-content-center align-items-center">
+            <Col md={12} className="p-3">
+              <h2 id="Register">
+                <FormattedMessage id="title.register" defaultMessage="Register" />
+              </h2>
+              <Markdown value={site.registerDescription} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12} className="p-3">
+              <Form> {/* TODO add https://formspree.io/ */}
+                <Form.Group controlId="registerEmail">
+                  <FormattedMessage id="register.email" defaultMessage="Email">
+                    {(l_email) => (
+                      <>
+                        <Form.Label>
+                          {l_email}
+                        </Form.Label>
+                        <Form.Control type="email" placeholder={l_email} />
+                      </>
+                    )}
+                  </FormattedMessage>
+                </Form.Group>
 
-              <Form.Group controlId="registerName">
-                <FormattedMessage id="register.name" defaultMessage="Name">
-                  {(l_name) => (
-                    <>
-                      <Form.Label>
-                        {l_name}
-                      </Form.Label>
-                      <Form.Control type="input" placeholder={l_name} />
-                    </>
-                  )}
-                </FormattedMessage>
-              </Form.Group>
+                <Form.Group controlId="registerName">
+                  <FormattedMessage id="register.name" defaultMessage="Name">
+                    {(l_name) => (
+                      <>
+                        <Form.Label>
+                          {l_name}
+                        </Form.Label>
+                        <Form.Control type="input" placeholder={l_name} />
+                      </>
+                    )}
+                  </FormattedMessage>
+                </Form.Group>
 
-              <Form.Row>
-                <Form.Group as={Col} controlId="registerPreferredTime">
+                <Form.Row>
+                  <Form.Group as={Col} controlId="registerPreferredTime">
+                    <Form.Label>
+                      <FormattedMessage id="register.preferred_time" defaultMessage="Preferred time" />
+                    </Form.Label>
+                    <Form.Control as="select">
+                      <option value=""></option>
+                      <FormattedMessage id="register.preferred_time.morning" defaultMessage="Morning">
+                        {(o) => <option value={o}>{o}</option>}
+                      </FormattedMessage>
+                      <FormattedMessage id="register.preferred_time.afternoon" defaultMessage="Afternoon">
+                        {(o) => <option value={o}>{o}</option>}
+                      </FormattedMessage>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="registerPreferredeDay">
+                    <Form.Label>
+                      <FormattedMessage id="register.preferred_day" defaultMessage="Preferred day" />
+                    </Form.Label>
+                    <Form.Control as="select">
+                      <option value=""></option>
+                      <FormattedMessage id="register.preferred_day.workday" defaultMessage="Workday">
+                        {(o) => <option value={o}>{o}</option>}
+                      </FormattedMessage>
+                      <FormattedMessage id="register.preferred_day.weekend" defaultMessage="Weekend">
+                        {(o) => <option value={o}>{o}</option>}
+                      </FormattedMessage>
+                    </Form.Control>
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Group controlId="registerFaculty">
                   <Form.Label>
-                    <FormattedMessage id="register.preferred_time" defaultMessage="Preferred time" />
+                    <FormattedMessage id="register.faculty" defaultMessage="Faculty" />
                   </Form.Label>
+                  <Link to={`/faculties/#Faculties`} class="float-right">
+                    <FormattedMessage id="register.faculty.help" defaultMessage="Do you need help with selection?" />
+                  </Link>
                   <Form.Control as="select">
                     <option value=""></option>
-                    <FormattedMessage id="register.preferred_time.morning" defaultMessage="Morning">
-                      {(o) => <option value={o}>{o}</option>}
-                    </FormattedMessage>
-                    <FormattedMessage id="register.preferred_time.afternoon" defaultMessage="Afternoon">
-                      {(o) => <option value={o}>{o}</option>}
-                    </FormattedMessage>
+                    {faculties.edges.map((item, index) => {
+                      return (
+                        <option value={item.node.title}>{item.node.title}</option>
+                      );
+                    })}
                   </Form.Control>
                 </Form.Group>
-                <Form.Group as={Col} controlId="registerPreferredeDay">
-                  <Form.Label>
-                    <FormattedMessage id="register.preferred_day" defaultMessage="Preferred day" />
-                  </Form.Label>
-                  <Form.Control as="select">
-                    <option value=""></option>
-                    <FormattedMessage id="register.preferred_day.workday" defaultMessage="Workday">
-                      {(o) => <option value={o}>{o}</option>}
-                    </FormattedMessage>
-                    <FormattedMessage id="register.preferred_day.weekend" defaultMessage="Weekend">
-                      {(o) => <option value={o}>{o}</option>}
-                    </FormattedMessage>
-                  </Form.Control>
-                </Form.Group>
-              </Form.Row>
 
-              <Form.Group controlId="registerFaculty">
-                <Form.Label>
-                  <FormattedMessage id="register.faculty" defaultMessage="Faculty" />
-                </Form.Label>
-                <Link to={`/faculties/#Faculties`} class="float-right">
-                  <FormattedMessage id="register.faculty.help" defaultMessage="Do you need help with selection?" />
-                </Link>
-                <Form.Control as="select">
-                  <option value=""></option>
-                  {faculties.edges.map((item, index) => {
-                    return (
-                      <option value={item.node.title}>{item.node.title}</option>
-                    );
-                  })}
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Row>
-                <Col md={12} className="text-justify">
-                  <Form.Text>
-                    <FormattedMessage id="register.consent" />
-                  </Form.Text>
-                </Col>
-                <Col md={12} className="text-center">
-                  <Button variant="primary" type="submit" size="lg">
-                    <FormattedMessage id="register.submit" defaultMessage="Submit" />
-                  </Button>
-                </Col>
-              </Form.Row>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+                <Form.Row>
+                  <Col md={12} className="text-justify">
+                    <Form.Text>
+                      <FormattedMessage id="register.consent" />
+                    </Form.Text>
+                  </Col>
+                  <Col md={12} className="text-center">
+                    <Button variant="danger" type="submit" size="lg">
+                      <FormattedMessage id="register.submit" defaultMessage="Submit" />
+                    </Button>
+                  </Col>
+                </Form.Row>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
