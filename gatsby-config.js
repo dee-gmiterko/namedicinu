77c4a7308543efd8c65ea219d718183434e3d94a
Dominik Gmiterko <d.gmiterko@gmail.com>
2021-04-07 00:09:11 +0200

    Update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,33 +4,40 @@ if (process.env.ENVIRONMENT !== "production") {
   dotenv.config();
 }
 
-const { spaceId, accessToken, locale } = process.env;
+let { spaceId, accessToken, locale } = process.env;
 
-console.log("Using locale "+(locale || "sk"));
+if (!locale) {
+  locale = "sk";
+}
+
+console.log(`Using locale ${locale}`);
 
 module.exports = {
+  flags: { PRESERVE_WEBPACK_CACHE: true },
   siteMetadata: {
     title: `Na medicinu`,
     description: `Course site`,
-    author: `Dominik Gmiterko <d.gmiterko@gmail.com>`
+    author: `Dominik Gmiterko <d.gmiterko@gmail.com>`,
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-global-context',
-      options: {
-        context: {
-          locale: locale || "sk"
-        }
-      }
-    },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        name: `Na medicínu`,
+        short_name: `Na medicínu`,
+        start_url: `/`,
+        background_color: `#fcf2e4`,
+        theme_color: `#ffc250`,
+        display: `minimal-ui`,
+        icon: `src/images/fev_icon.png`,
+      },
     },
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: "gatsby-source-contentful",
       options: {
@@ -38,21 +45,15 @@ module.exports = {
         accessToken
       }
     },
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-global-context',
       options: {
-        name: `Na medicinu`,
-        short_name: `Na medicinu`,
-        start_url: `/`,
-        background_color: `#663399`, // TODO
-        theme_color: `#333`, // TODO
-        icon: `src/images/fev_icon.png` // TODO
+        context: {
+          locale: locale
+        }
       }
-    }, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
-    `gatsby-plugin-offline`
-  ]
-};
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
+  ],
+}
